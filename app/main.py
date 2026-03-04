094bc4b961a0910b6ef4e4abdda3dbfb3978e875
Ashish Thapa <aacict@gmail.com>
2026-03-04 17:14:28 -0600

    added prometheus monitoring docker connected to api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.core.logging import setup_logging
+from prometheus_fastapi_instrumentator import Instrumentator
 import logging
 
 setup_logging()
@@ -8,7 +9,16 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+#Initializing the prometheus instrumentator
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
+
 @app.get("/")
+async def root():
+    logger.info("Root endpoint called....")
+    return {"message": "Hello with FastAPI and Prometheus!"}
+
+@app.get("/health")
 async def health_check():
     logger.info("Health check endpoint called....")
     return {"status": "ok"}
