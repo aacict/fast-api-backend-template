6d97d1e5006625066f64e9bb8d32187425876c17
Ashish Thapa <aacict@gmail.com>
2026-03-04 19:04:59 -0600

    added sentry to project to trace errors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
+from app.core.config import settings
 from fastapi import FastAPI
 from app.core.logging import setup_logging
 from prometheus_fastapi_instrumentator import Instrumentator
 import logging
+import sentry_sdk
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
+if settings.sentry_dsn:
+    sentry_sdk.init(dsn=settings.sentry_dsn, send_default_pii=True)
 
 setup_logging()
 
@@ -13,6 +19,9 @@ app = FastAPI()
 instrumentator = Instrumentator()
 instrumentator.instrument(app).expose(app)
 
+# Adding Sentry middleware to capture exceptions and performance data
+app.add_middleware(SentryAsgiMiddleware)
+
 @app.get("/")
 async def root():
     logger.info("Root endpoint called....")
@@ -22,3 +31,7 @@ async def root():
 async def health_check():
     logger.info("Health check endpoint called....")
     return {"status": "ok"}
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
